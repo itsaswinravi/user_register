70aada209af2b31de303d2c2f034e7c223480e0c
itsaswinravi <achu322001@gmail.com>
2024-08-02 10:26:57 +0530

    added cors

--- nodejs_jwt_auth-main/config/allowedOrigins.js
+++ nodejs_jwt_auth-main/config/allowedOrigins.js
@@ -2,7 +2,10 @@ const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://xamprepp.netlify.app',
+    /https:\/\/deploy-preview-\d+--xamprepp\.netlify\.app/,
 ];
 
 module.exports = allowedOrigins;
